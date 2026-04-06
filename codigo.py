d131e56bae39895c07f74de1e19c8092502f518f
thiago896 <thiago.alcantara@alunos.ifsuldeminas.edu.br>
2026-04-06 10:49:00 -0300

    aula5

---
+++ codigo.py
@@ -0,0 +1,15 @@
+import pyautogui
+
+#clica duas vezes em uma coordenada x, y
+pyautogui.click(290, 455, clicks = 1, duration = 0.01)
+
+pyautogui.click(130, 400, clicks = 1, duration = 0.01)
+
+pyautogui.click(370, 520, clicks = 1, duration = 0.01)
+
+pyautogui.click(290, 455, clicks = 1, duration = 0.01)
+
+pyautogui.click(130, 400, clicks = 1, duration = 0.01)
+
+pyautogui.click(376, 560, clicks = 1, duration = 0.01)
+
